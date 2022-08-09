2adf3cf82914a1f96f07b68d5bf78ffdf1277cc1
rkskekzzz <wkdlfflxh@naver.com>
2022-08-10 04:29:13 +0900

    🎨 루트 패스 protected routing 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
 import { SignIn, Todo } from './pages/';
 import './App.css';
+
+const ProtectedRoute = () => {
+  if (!window.localStorage.getItem('todoAuthToken')) {
+    return <Navigate to={'/auth'} replace />;
+  }
+  return <Outlet />;
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<SignIn />} />
-        <Route path="/" element={<Todo />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Todo />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
