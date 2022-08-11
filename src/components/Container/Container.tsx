5788c55420643400f254abc84cb4d01acd1844f2
rkskekzzz <wkdlfflxh@naver.com>
2022-08-12 01:03:50 +0900

    🎨 공통 컴포넌트 분리

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import styled from '@emotion/styled';
+
+const Paper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  > div {
+    overflow: scroll;
+  }
+`;
+
+interface ContainerProps {
+  children: JSX.Element[] | JSX.Element;
+}
+
+const Container = ({ children }: ContainerProps) => {
+  return (
+    <Paper>
+      <Box width={500} height="100vh" alignItems="center">
+        {children}
+      </Box>
+    </Paper>
+  );
+};
+
+export default Container;
