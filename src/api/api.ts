5af882ad8f9b98c786b318e28e5a9d4fc20e7ea0
rkskekzzz <wkdlfflxh@naver.com>
2022-08-12 00:15:23 +0900

    🔥 불필요한 axios import 제거

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
-export function makeApiPath(path: string) {
+export default function makeApiPath(path: string) {
   return `${process.env.REACT_APP_API_URL}${path}`;
 }
